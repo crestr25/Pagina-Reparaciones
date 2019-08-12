1626995de96b58e60d61d01ebaccf2029d862778
Carlos Manuel Restrepo <crestr25@gmail.com>
2019-08-12 09:55:45 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// SET UP EXPRESS
+var express = require("express");
+var app = express()
+var port = 3000
+// SET UP BODY PARSER
+// SET UP DATABASE(MONGODB, MONGOOSE)
+
+// ROUTES
+
+// "/"
+
+// "/NUEVA" GET, POST
+
+// "/CONSULTAR" GET, POST
+
+app.listen(port, function(){
+    console.log("Magic Happens on port " + port)
+})
